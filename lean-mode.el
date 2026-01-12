edb23d32b3ee3346583d40cbeaa545347cf1bd7d
Trevor Murphy <trevor.m.murphy@gmail.com>
2026-01-11 18:50:41 -0800

    Move font-lock into the main file.

--- lean-mode.el
+++ lean-mode.el
@@ -54,7 +54,6 @@
 (require 'project)
 (require 'rx)
 (require 'seq)
-(require 'lean-font-lock)
 (require 'lean-ts)
 
 ;; forward declarations
@@ -82,9 +81,193 @@ If you change this setting you will need to restart the major mode."
              (add-hook 'lean-mode-hook #'lean-ts-setup)
            (remove-hook 'lean-mode-hook #'lean-ts-setup))))
 
+;;; Internal Variables
+
+;;;; Syntax:
+
+(defconst lean-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    ;; Matching parens
+    (modify-syntax-entry ?\[ "(]" st)
+    (modify-syntax-entry ?\] ")[" st)
+    (modify-syntax-entry ?\{ "(}" st)
+    (modify-syntax-entry ?\} "){" st)
+
+    ;; comment
+    (modify-syntax-entry ?/ ". 14nb" st)
+    (modify-syntax-entry ?- ". 123" st)
+    (modify-syntax-entry ?\n ">" st)
+    (modify-syntax-entry ?« "<" st)
+    (modify-syntax-entry ?» ">" st)
+
+    ;; Word constituent
+    (dolist (it (list ?a ?b ?c ?d ?e ?f ?g ?h ?i ?j ?k ?l ?m ?n ?o ?p
+                      ?q ?r ?s ?t ?u ?v ?w ?x ?y ?z ?A ?B ?C ?D ?E ?F
+                      ?G ?H ?I ?J ?K ?L ?M ?N ?O ?P ?Q ?R ?S ?T ?U ?V
+                      ?W ?X ?Y ?Z
+
+                      ?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9
+
+                      ?α ?β ?γ ?δ ?ε ?ζ ?η ?θ ?ι ?κ ;;?λ ?μ ?ν ?ξ ?ο
+                      ?π ?ρ ?ς ?σ ?τ ?υ ?φ ?χ ?ψ ?ω
+
+                      ?ϊ ?ϋ ?ό ?ύ ?ώ ?Ϗ ?ϐ ?ϑ ?ϒ ?ϓ ?ϔ ?ϕ ?ϖ ?ϗ ?Ϙ ?ϙ
+                      ?Ϛ ?ϛ ?Ϝ ?ϝ ?Ϟ ?ϟ ?Ϡ ?ϡ ?Ϣ ?ϣ ?Ϥ ?ϥ ?Ϧ ?ϧ ?Ϩ ?ϩ
+                      ?Ϫ ?ϫ ?Ϭ ?ϭ ?Ϯ ?ϯ ?ϰ ?ϱ ?ϲ ?ϳ ?ϴ ?ϵ ?϶ ?Ϸ ?ϸ ?Ϲ
+                      ?Ϻ ?ϻ
+
+                      ?ἀ ?ἁ ?ἂ ?ἃ ?ἄ ?ἅ ?ἆ ?ἇ ?Ἀ ?Ἁ ?Ἂ ?Ἃ ?Ἄ ?Ἅ ?Ἆ ?Ἇ
+                      ?ἐ ?ἑ ?ἒ ?ἓ ?ἔ ?ἕ ?἖ ?἗ ?Ἐ ?Ἑ ?Ἒ ?Ἓ ?Ἔ ?Ἕ ?἞ ?἟
+                      ?ἠ ?ἡ ?ἢ ?ἣ ?ἤ ?ἥ ?ἦ ?ἧ ?Ἠ ?Ἡ ?Ἢ ?Ἣ ?Ἤ ?Ἥ ?Ἦ ?Ἧ
+                      ?ἰ ?ἱ ?ἲ ?ἳ ?ἴ ?ἵ ?ἶ ?ἷ ?Ἰ ?Ἱ ?Ἲ ?Ἳ ?Ἴ ?Ἵ ?Ἶ ?Ἷ
+                      ?ὀ ?ὁ ?ὂ ?ὃ ?ὄ ?ὅ ?὆ ?὇ ?Ὀ ?Ὁ ?Ὂ ?Ὃ ?Ὄ ?Ὅ ?὎ ?὏
+                      ?ὐ ?ὑ ?ὒ ?ὓ ?ὔ ?ὕ ?ὖ ?ὗ ?὘ ?Ὑ ?὚ ?Ὓ ?὜ ?Ὕ ?὞ ?Ὗ
+                      ?ὠ ?ὡ ?ὢ ?ὣ ?ὤ ?ὥ ?ὦ ?ὧ ?Ὠ ?Ὡ ?Ὢ ?Ὣ ?Ὤ ?Ὥ ?Ὦ ?Ὧ
+                      ?ὰ ?ά ?ὲ ?έ ?ὴ ?ή ?ὶ ?ί ?ὸ ?ό ?ὺ ?ύ ?ὼ ?ώ ?὾ ?὿
+                      ?ᾀ ?ᾁ ?ᾂ ?ᾃ ?ᾄ ?ᾅ ?ᾆ ?ᾇ ?ᾈ ?ᾉ ?ᾊ ?ᾋ ?ᾌ ?ᾍ ?ᾎ ?ᾏ
+                      ?ᾐ ?ᾑ ?ᾒ ?ᾓ ?ᾔ ?ᾕ ?ᾖ ?ᾗ ?ᾘ ?ᾙ ?ᾚ ?ᾛ ?ᾜ ?ᾝ ?ᾞ ?ᾟ
+                      ?ᾠ ?ᾡ ?ᾢ ?ᾣ ?ᾤ ?ᾥ ?ᾦ ?ᾧ ?ᾨ ?ᾩ ?ᾪ ?ᾫ ?ᾬ ?ᾭ ?ᾮ ?ᾯ
+                      ?ᾰ ?ᾱ ?ᾲ ?ᾳ ?ᾴ ?᾵ ?ᾶ ?ᾷ ?Ᾰ ?Ᾱ ?Ὰ ?Ά ?ᾼ ?᾽ ?ι ?᾿
+                      ?῀ ?῁ ?ῂ ?ῃ ?ῄ ?῅ ?ῆ ?ῇ ?Ὲ ?Έ ?Ὴ ?Ή ?ῌ ?῍ ?῎ ?῏
+                      ?ῐ ?ῑ ?ῒ ?ΐ ?῔ ?῕ ?ῖ ?ῗ ?Ῐ ?Ῑ ?Ὶ ?Ί ?῜ ?῝ ?῞ ?῟
+                      ?ῠ ?ῡ ?ῢ ?ΰ ?ῤ ?ῥ ?ῦ ?ῧ ?Ῠ ?Ῡ ?Ὺ ?Ύ ?Ῥ ?῭ ?΅ ?`
+                      ?῰ ?῱ ?ῲ ?ῳ ?ῴ ?῵ ?ῶ ?ῷ ?Ὸ ?Ό ?Ὼ ?Ώ ?ῼ ?´ ?῾
+
+                      ?℀ ?℁ ?ℂ ?℃ ?℄ ?℅ ?℆ ?ℇ ?℈ ?℉ ?ℊ ?ℋ ?ℌ ?ℍ ?ℎ ?ℏ
+                      ?ℐ ?ℑ ?ℒ ?ℓ ?℔ ?ℕ ?№ ?℗ ?℘ ?ℙ ?ℚ ?ℛ ?ℜ ?ℝ ?℞ ?℟
+                      ?℠ ?℡ ?™ ?℣ ?ℤ ?℥ ?Ω ?℧ ?ℨ ?℩ ?K ?Å ?ℬ ?ℭ ?℮ ?ℯ
+                      ?ℰ ?ℱ ?Ⅎ ?ℳ ?ℴ ?ℵ ?ℶ ?ℷ ?ℸ ?ℹ ?℺ ?℻ ?ℼ ?ℽ ?ℾ ?ℿ
+                      ?⅀ ?⅁ ?⅂ ?⅃ ?⅄ ?ⅅ ?ⅆ ?ⅇ ?ⅈ ?ⅉ ?⅊ ?⅋ ?⅌ ?⅍ ?ⅎ ?⅏
+
+                      ?₁ ?₂ ?₃ ?₄ ?₅ ?₆ ?₇ ?₈ ?₉ ?₀ ?ₐ ?ₑ ?ₒ ?ₓ ?ₔ ?ₕ
+                      ?ₖ ?ₗ ?ₘ ?ₙ ?ₚ ?ₛ ?ₜ ?' ?_ ?! ??))
+      (modify-syntax-entry it "w" st))
+
+    ;; Lean operator chars
+    (dolist (it (string-to-list "#$%&*+<=>@^|~:"))
+      (modify-syntax-entry it "." st))
+
+    ;; Whitespace is whitespace
+    (modify-syntax-entry ?\  " " st)
+    (modify-syntax-entry ?\t " " st)
+
+    ;; Strings
+    (modify-syntax-entry ?\" "\"" st)
+    (modify-syntax-entry ?\\ "/" st)
+
+    st))
+
+;;;; Font Locking:
+
+(defconst lean-declarations
+  '("instance" "structure" "class" "theorem" "axiom" "lemma" "definition" "def" "constant")
+  "Lean declarations.")
+(defconst lean-declarations-regexp
+  (rx word-start
+      (group (eval (append '(or "inductive"
+                                (group "class" (zero-or-more whitespace) "inductive"))
+                           lean-declarations)))
+      word-end (zero-or-more whitespace)
+      (group (zero-or-more "{" (zero-or-more (not (any "}"))) "}" (zero-or-more whitespace)))
+      (zero-or-more whitespace)
+      (group (zero-or-more (not (any " \t\n\r{(["))))))
+
+(defconst lean-keywords
+  '("import" "prelude" "protected" "private" "noncomputable"
+    "unsafe" "partial" "renaming" "hiding" "begin" "constant"
+    "variable" "variables" "theorem" "example" "abbrev"
+    "open" "export" "axiom" "inductive" "with"
+    "structure" "universe" "universes" "hide"
+    "precedence" "match_syntax" "match" "nomatch" "infix" "infixl" "infixr" "notation" "postfix" "prefix" "instance"
+    "end" "this" "using" "using_well_founded" "namespace" "section"
+    "attribute" "local" "set_option" "extends" "include" "class"
+    "attributes" "raw" "have" "show" "suffices" "by" "in" "at" "do" "let" "for" "unless" "break" "continue"
+    "try" "catch" "finally" "where" "rec" "mut" "forall" "fun"
+    "exists" "if" "then" "else" "from" "init_quot" "return"
+    "mutual" "def" "run_cmd" "declare_syntax_cat" "syntax" "macro_rules" "macro" "scoped" "elab"
+    "initialize" "builtin_initialize" "register_builtin_option" "induction" "cases" "generalizing" "unif_hint" "deriving")
+  "Lean keywords ending with `word' (not symbol).")
+(defconst lean-keywords-regexp
+  (rx word-start (eval (cons 'or lean-keywords)) word-end))
+
+(defconst lean-constants
+  '("#" "@" "!" "$" "->" "∼" "↔" "/" "==" "=" ":=" "<->" "/\\" "\\/" "∧" "∨"
+    "≠" "<" ">" "≤" "≥" "¬" "<=" ">=" "⁻¹" "⬝" "▸" "+" "*" "-" "/" "λ"
+    "→" "∃" "∀" "∘" "×" "Σ" "Π" "~" "||" "&&" "≃" "≡" "≅"
+    "ℕ" "ℤ" "ℚ" "ℝ" "ℂ" "𝔸"
+    "⬝e" "⬝i" "⬝o" "⬝op" "⬝po" "⬝h" "⬝v" "⬝hp" "⬝vp" "⬝ph" "⬝pv" "⬝r" "◾" "◾o"
+    "∘n" "∘f" "∘fi" "∘nf" "∘fn" "∘n1f" "∘1nf" "∘f1n" "∘fn1"
+    "^c" "≃c" "≅c" "×c" "×f" "×n" "+c" "+f" "+n" "ℕ₋₂")
+  "Lean constants.")
+(defconst lean-constants-regexp (regexp-opt lean-constants))
+
+(defconst lean-numerals-regexp
+  (rx word-start
+      (one-or-more digit) (optional (and "." (zero-or-more digit)))
+      word-end))
+
+(defconst lean-warnings '("sorry") "Lean warnings.")
+(defconst lean-warnings-regexp
+  (rx word-start (eval (cons 'or lean-warnings)) word-end))
+
+(defconst lean-debugging '("unreachable!" "panic!" "assert!" "dbg_trace") "Lean debugging.")
+(defconst lean-debugging-regexp
+  (rx word-start (eval (cons 'or lean-debugging)) word-end))
+
+(defconst lean4-font-lock-defaults
+  `((;; attributes
+     (,(rx word-start "attribute" word-end (zero-or-more whitespace)
+           (group (one-or-more "[" (zero-or-more (not (any "]"))) "]"
+                               (zero-or-more whitespace))))
+      (1 'font-lock-preprocessor-face))
+     (,(rx (group "@[" (zero-or-more (not (any "]"))) "]"))
+      (1 'font-lock-preprocessor-face))
+     (,(rx (group "#" (or "eval" "print" "reduce" "help" "check" "lang" "check_failure" "synth")))
+      (1 'font-lock-keyword-face))
+     ;; mutual definitions "names"
+     (,(rx word-start
+           "mutual"
+           word-end
+           (zero-or-more whitespace)
+           word-start
+           (or "inductive" "definition" "def")
+           word-end
+           (group (zero-or-more (not (any " \t\n\r{([,")))
+                  (zero-or-more (zero-or-more whitespace) "," (zero-or-more whitespace)
+                                (not (any " \t\n\r{([,")))))
+      (1 'font-lock-function-name-face))
+     ;; declarations
+     (,lean-declarations-regexp (4 'font-lock-function-name-face))
+     ;; Constants which have a keyword as subterm
+     (,(rx (or "∘if")) . 'font-lock-constant-face)
+     ;; Keywords
+     ("\\(set_option\\)[ \t]*\\([^ \t\n]*\\)" (2 'font-lock-constant-face))
+     (,lean-keywords-regexp . 'font-lock-keyword-face)
+     (,(rx word-start (group "example") ".") (1 'font-lock-keyword-face))
+     (,(rx (or "∎")) . 'font-lock-keyword-face)
+     ;; Types
+     (,(rx word-start (or "Prop" "Type" "Type*" "Sort" "Sort*") symbol-end) . 'font-lock-type-face)
+     (,(rx word-start (group (or "Prop" "Type" "Sort")) ".") (1 'font-lock-type-face))
+     ;; String
+     ("\"[^\"]*\"" . 'font-lock-string-face)
+     ;; Debugging builtins
+     (,lean-debugging-regexp . 'font-lock-warning-face)
+     ;; ;; Constants
+     (,lean-constants-regexp . 'font-lock-constant-face)
+     (,lean-numerals-regexp . 'font-lock-constant-face)
+     ;; place holder
+     (,(rx symbol-start "_" symbol-end) . 'font-lock-preprocessor-face)
+     ;; warnings
+     (,lean-warnings-regexp . 'font-lock-warning-face)
+     ;; escaped identifiers
+     (,(rx (and (group "«") (group (one-or-more (not (any "»")))) (group "»")))
+      (1 font-lock-comment-face t)
+      (2 nil t)
+      (3 font-lock-comment-face t)))))
+
 ;;;; Utility Functions
 
-;;;; Comments
+;;;; Comments:
 
 (defconst lean--line-comment-region-alist
   '((comment-start . "-- ") (comment-end . "") (comment-style . indent))
@@ -97,18 +280,6 @@ If you change this setting you will need to restart the major mode."
 (defconst lean--declaration-comment-region-alist
   '((comment-start . "/--") (comment-end . "-/") (comment-style . extra-line))
   "Make `comment-region' wrap a region with a declaration block comment.")
-
-(defun lean--set-comment-variables ()
-  (setq-local comment-start "-- ")
-  (setq-local comment-start-skip "\\(?:--\\|/-!?\\)[[:space:]]*")
-  (setq-local comment-end "")
-  (setq-local comment-end-skip "[[:space:]]*\\(?:-/\\|\\s>\\)")
-  (setq-local comment-padding 1)
-  (setq-local comment-insert-comment-function #'lean-insert-comment)
-  (setq-local comment-quote-nested nil)
-  (setq-local comment-style 'indent)
-  (setq-local comment-use-syntax t)
-  (setq-local parse-sexp-ignore-comments t))
 
 ;;; why isn't there a builtin for this already?
 ;;; for now just reuse evil, but need a better long term solution
@@ -340,90 +511,31 @@ through various block comment styles if called repeatedly."
 (defvar-keymap lean-mode-map
   "<remap> <display-local-help>" #'eldoc-doc-buffer)
 
-(defconst lean-mode-syntax-table
-  (let ((st (make-syntax-table)))
-    ;; Matching parens
-    (modify-syntax-entry ?\[ "(]" st)
-    (modify-syntax-entry ?\] ")[" st)
-    (modify-syntax-entry ?\{ "(}" st)
-    (modify-syntax-entry ?\} "){" st)
-
-    ;; comment
-    (modify-syntax-entry ?/ ". 14nb" st)
-    (modify-syntax-entry ?- ". 123" st)
-    (modify-syntax-entry ?\n ">" st)
-    (modify-syntax-entry ?« "<" st)
-    (modify-syntax-entry ?» ">" st)
-
-    ;; Word constituent
-    (dolist (it (list ?a ?b ?c ?d ?e ?f ?g ?h ?i ?j ?k ?l ?m ?n ?o ?p
-                      ?q ?r ?s ?t ?u ?v ?w ?x ?y ?z ?A ?B ?C ?D ?E ?F
-                      ?G ?H ?I ?J ?K ?L ?M ?N ?O ?P ?Q ?R ?S ?T ?U ?V
-                      ?W ?X ?Y ?Z
-
-                      ?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9
-
-                      ?α ?β ?γ ?δ ?ε ?ζ ?η ?θ ?ι ?κ ;;?λ ?μ ?ν ?ξ ?ο
-                      ?π ?ρ ?ς ?σ ?τ ?υ ?φ ?χ ?ψ ?ω
-
-                      ?ϊ ?ϋ ?ό ?ύ ?ώ ?Ϗ ?ϐ ?ϑ ?ϒ ?ϓ ?ϔ ?ϕ ?ϖ ?ϗ ?Ϙ ?ϙ
-                      ?Ϛ ?ϛ ?Ϝ ?ϝ ?Ϟ ?ϟ ?Ϡ ?ϡ ?Ϣ ?ϣ ?Ϥ ?ϥ ?Ϧ ?ϧ ?Ϩ ?ϩ
-                      ?Ϫ ?ϫ ?Ϭ ?ϭ ?Ϯ ?ϯ ?ϰ ?ϱ ?ϲ ?ϳ ?ϴ ?ϵ ?϶ ?Ϸ ?ϸ ?Ϲ
-                      ?Ϻ ?ϻ
-
-                      ?ἀ ?ἁ ?ἂ ?ἃ ?ἄ ?ἅ ?ἆ ?ἇ ?Ἀ ?Ἁ ?Ἂ ?Ἃ ?Ἄ ?Ἅ ?Ἆ ?Ἇ
-                      ?ἐ ?ἑ ?ἒ ?ἓ ?ἔ ?ἕ ?἖ ?἗ ?Ἐ ?Ἑ ?Ἒ ?Ἓ ?Ἔ ?Ἕ ?἞ ?἟
-                      ?ἠ ?ἡ ?ἢ ?ἣ ?ἤ ?ἥ ?ἦ ?ἧ ?Ἠ ?Ἡ ?Ἢ ?Ἣ ?Ἤ ?Ἥ ?Ἦ ?Ἧ
-                      ?ἰ ?ἱ ?ἲ ?ἳ ?ἴ ?ἵ ?ἶ ?ἷ ?Ἰ ?Ἱ ?Ἲ ?Ἳ ?Ἴ ?Ἵ ?Ἶ ?Ἷ
-                      ?ὀ ?ὁ ?ὂ ?ὃ ?ὄ ?ὅ ?὆ ?὇ ?Ὀ ?Ὁ ?Ὂ ?Ὃ ?Ὄ ?Ὅ ?὎ ?὏
-                      ?ὐ ?ὑ ?ὒ ?ὓ ?ὔ ?ὕ ?ὖ ?ὗ ?὘ ?Ὑ ?὚ ?Ὓ ?὜ ?Ὕ ?὞ ?Ὗ
-                      ?ὠ ?ὡ ?ὢ ?ὣ ?ὤ ?ὥ ?ὦ ?ὧ ?Ὠ ?Ὡ ?Ὢ ?Ὣ ?Ὤ ?Ὥ ?Ὦ ?Ὧ
-                      ?ὰ ?ά ?ὲ ?έ ?ὴ ?ή ?ὶ ?ί ?ὸ ?ό ?ὺ ?ύ ?ὼ ?ώ ?὾ ?὿
-                      ?ᾀ ?ᾁ ?ᾂ ?ᾃ ?ᾄ ?ᾅ ?ᾆ ?ᾇ ?ᾈ ?ᾉ ?ᾊ ?ᾋ ?ᾌ ?ᾍ ?ᾎ ?ᾏ
-                      ?ᾐ ?ᾑ ?ᾒ ?ᾓ ?ᾔ ?ᾕ ?ᾖ ?ᾗ ?ᾘ ?ᾙ ?ᾚ ?ᾛ ?ᾜ ?ᾝ ?ᾞ ?ᾟ
-                      ?ᾠ ?ᾡ ?ᾢ ?ᾣ ?ᾤ ?ᾥ ?ᾦ ?ᾧ ?ᾨ ?ᾩ ?ᾪ ?ᾫ ?ᾬ ?ᾭ ?ᾮ ?ᾯ
-                      ?ᾰ ?ᾱ ?ᾲ ?ᾳ ?ᾴ ?᾵ ?ᾶ ?ᾷ ?Ᾰ ?Ᾱ ?Ὰ ?Ά ?ᾼ ?᾽ ?ι ?᾿
-                      ?῀ ?῁ ?ῂ ?ῃ ?ῄ ?῅ ?ῆ ?ῇ ?Ὲ ?Έ ?Ὴ ?Ή ?ῌ ?῍ ?῎ ?῏
-                      ?ῐ ?ῑ ?ῒ ?ΐ ?῔ ?῕ ?ῖ ?ῗ ?Ῐ ?Ῑ ?Ὶ ?Ί ?῜ ?῝ ?῞ ?῟
-                      ?ῠ ?ῡ ?ῢ ?ΰ ?ῤ ?ῥ ?ῦ ?ῧ ?Ῠ ?Ῡ ?Ὺ ?Ύ ?Ῥ ?῭ ?΅ ?`
-                      ?῰ ?῱ ?ῲ ?ῳ ?ῴ ?῵ ?ῶ ?ῷ ?Ὸ ?Ό ?Ὼ ?Ώ ?ῼ ?´ ?῾
-
-                      ?℀ ?℁ ?ℂ ?℃ ?℄ ?℅ ?℆ ?ℇ ?℈ ?℉ ?ℊ ?ℋ ?ℌ ?ℍ ?ℎ ?ℏ
-                      ?ℐ ?ℑ ?ℒ ?ℓ ?℔ ?ℕ ?№ ?℗ ?℘ ?ℙ ?ℚ ?ℛ ?ℜ ?ℝ ?℞ ?℟
-                      ?℠ ?℡ ?™ ?℣ ?ℤ ?℥ ?Ω ?℧ ?ℨ ?℩ ?K ?Å ?ℬ ?ℭ ?℮ ?ℯ
-                      ?ℰ ?ℱ ?Ⅎ ?ℳ ?ℴ ?ℵ ?ℶ ?ℷ ?ℸ ?ℹ ?℺ ?℻ ?ℼ ?ℽ ?ℾ ?ℿ
-                      ?⅀ ?⅁ ?⅂ ?⅃ ?⅄ ?ⅅ ?ⅆ ?ⅇ ?ⅈ ?ⅉ ?⅊ ?⅋ ?⅌ ?⅍ ?ⅎ ?⅏
-
-                      ?₁ ?₂ ?₃ ?₄ ?₅ ?₆ ?₇ ?₈ ?₉ ?₀ ?ₐ ?ₑ ?ₒ ?ₓ ?ₔ ?ₕ
-                      ?ₖ ?ₗ ?ₘ ?ₙ ?ₚ ?ₛ ?ₜ ?' ?_ ?! ??))
-      (modify-syntax-entry it "w" st))
-
-    ;; Lean operator chars
-    (dolist (it (string-to-list "#$%&*+<=>@^|~:"))
-      (modify-syntax-entry it "." st))
-
-    ;; Whitespace is whitespace
-    (modify-syntax-entry ?\  " " st)
-    (modify-syntax-entry ?\t " " st)
-
-    ;; Strings
-    (modify-syntax-entry ?\" "\"" st)
-    (modify-syntax-entry ?\\ "/" st)
-
-    st))
-
 ;;;###autoload
 (define-derived-mode lean-mode prog-mode "Lean"
   "Major mode for Lean.
 
 \\{lean-mode-map}"
-  ;; Preparations:
+  :syntax-table lean-mode-syntax-table
+
+  ;; Comments:
+  (setq-local comment-start "-- ")
+  (setq-local comment-start-skip "\\(?:--\\|/-!?\\)[[:space:]]*")
+  (setq-local comment-end "")
+  (setq-local comment-end-skip "[[:space:]]*\\(?:-/\\|\\s>\\)")
+  (setq-local comment-style 'indent)
+  (setq-local comment-padding 1)
+  (setq-local comment-insert-comment-function #'lean-insert-comment)
+  (setq-local comment-quote-nested nil)
+  (setq-local comment-use-syntax t)
+  (setq-local parse-sexp-ignore-comments t)
 
   ;; Navigation:
 
   ;; Paragraphs and filling:
 
-  ;; Font-lock:
+  ;; Font Locking:
+  (setq-local font-lock-defaults lean4-font-lock-defaults)
 
   ;; Compile:
 
