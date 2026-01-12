9bd9fd845f62d94cbbd0248fb3d951ee8f010411
Trevor Murphy <trevor.m.murphy@gmail.com>
2026-01-11 21:56:55 -0800

    Move infoview into main file.
    Whoops, got a recursive loading issue.  It's because I'm trying to load
    main font lock variables into here, but trying to load this
    functionality into the main file.
    
    For now just throw everything into one enormous bucket.  If for some
    reason you need to split out font lock variables into their own file,
    just for shared imports, fine, but you can do that later.

--- lean-mode.el
+++ lean-mode.el
@@ -55,7 +55,6 @@
 (require 'rx)
 (require 'seq)
 
-(require 'lean-infoview)
 (require 'lean-lsp)
 
 ;;;; Autoloads and Forward Declarations
@@ -273,14 +272,21 @@ Currently so experimental that we don't support anything."
       (2 nil t)
       (3 font-lock-comment-face t)))))
 
-;;;; Infoview:
-
-;;; Inspired by nael, we hook into the eldoc mechanisms.  Inspired by
-;;; lean4-mode, we use a dedicated buffer for fontification etc.  From
-;;; my own work, seems best to use generics so we can swap backends
-;;; (eglot / lsp) as the user prefers.
-
-
+(defconst lean-infoview-font-lock-defaults
+  (let ((new-entries
+         `(;; Please add more after this:
+           (,(rx (group (+ symbol-start (+ (or word (char ?₁ ?₂ ?₃ ?₄ ?₅ ?₆ ?₇ ?₈ ?₉ ?₀))) symbol-end (* white))) ":")
+            (1 'font-lock-variable-name-face))
+           (,(rx white ":" white)
+            . 'font-lock-keyword-face)
+           (,(rx "⊢" white)
+            . 'font-lock-keyword-face)
+           (,(rx "[" (group "stale") "]")
+            (1 'font-lock-warning-face))
+           (,(rx line-start "No Goal" line-end)
+            . 'font-lock-constant-face)))
+        (inherited-entries (car lean-font-lock-defaults)))
+    `(,(-concat new-entries inherited-entries))))
 
 ;;;; Comments:
 
@@ -566,6 +572,90 @@ through various block comment styles if called repeatedly."
 
 ;; Lean language specification requires UTF-8 encoding.
 (modify-coding-system-alist 'file "\\.lean\\'" 'utf-8)
+
+;;;; Infoview:
+
+;;; Inspired by nael, we hook into the eldoc mechanisms.  Inspired by
+;;; lean4-mode, we use a dedicated buffer for fontification etc.  From
+;;; my own work, seems best to use generics so we can swap backends
+;;; (eglot / lsp) as the user prefers.
+
+(defgroup lean-infoview nil
+  "Repurpose `eldoc' to publish Lean LSP server responses."
+  :group 'lean
+  :prefix "lean-infoview-")
+
+(defface lean-infoview-section-face
+  '((t (:inherit font-lock-function-name-face :weight bold)))
+  "Face for section-headers of Lean infoview buffer."
+  :group 'lean-infoview)
+
+(define-derived-mode lean-infoview-mode prog-mode "LeanI"
+  "Major mode for the Lean Infoview buffer."
+  (setq-local font-lock-defaults lean-infoview-font-lock-defaults))
+
+(defvar lean-infoview--buffer-name "*Lean Infoview*"
+  "Name of buffer that is used to fontify responses from the LSP server.")
+
+(defun lean-infoview--goals (callback goals)
+  (if (= (length goals) 0)
+      ;; TODO: print the "goals accomplished" to an infoview buffer
+      (funcall callback nil)
+    (with-temp-buffer
+      (let (goal start end)
+        (with-demoted-errors "Error during fontlock: %s"
+          (insert (propertize "Tactic state:\n" 'face
+                              'lean-infoview-section-face)
+                  "\n")
+          (while goals
+            (setq goal (pop goals))
+            (insert "  ")
+            (unless start (setq start (point-marker)))
+            (insert goal)
+            (unless end (setq end (point-marker)))
+            (insert "\n\n"))
+          (delay-mode-hooks (funcall 'lean-infoview-mode))
+          (ignore-errors (font-lock-ensure)))
+        (funcall callback (buffer-string)
+                 :echo (buffer-substring start end))))))
+
+(defun lean-infoview--term-goal (callback goal)
+  (if (= (length goal) 0)
+      (funcall callback nil)
+    (with-temp-buffer
+      (with-demoted-errors "Error during fontlock: %s"
+        (insert (propertize "Expected type:\n" 'face
+                            'lean-infoview-section-face)
+                "\n")
+        (insert "  " (eglot--format-markup goal))
+        (delay-mode-hooks (funcall 'lean-infoview-mode))
+        (ignore-errors (font-lock-ensure)))
+      (funcall callback (buffer-string)
+               :echo 'skip))))
+
+(defun lean-infoview-goals (callback &rest _)
+  "ElDoc documentation function for the plain goals.
+
+ElDoc will provide CALLBACK.  See `eldoc-documentation-functions' for
+instructions on using CALLBACK to provide documentation info.
+
+The request target path is `$/lean/plainGoal' as documented here:
+https://leanprover-community.github.io/mathlib4_docs/Lean/Data/Lsp/Extra.html#Lean.Lsp.PlainGoal"
+  (lean-lsp--ensure-backend)
+  (lean-lsp--request-async lean-lsp--backend :$/lean/plainGoal
+                           #'lean-infoview--goals callback))
+
+(defun lean-infoview-term-goal (callback &rest _)
+  "ElDoc documentation function for the type of the term at point.
+
+ElDoc will provide CALLBACK.  See `eldoc-documentation-functions' for
+instructions on using CALLBACK to provide documentation info.
+
+The request target path is `$/lean/plainTermGoal' as documented here:
+https://leanprover-community.github.io/mathlib4_docs/Lean/Data/Lsp/Extra.html#Lean.Lsp.PlainTermGoal"
+  (lean-lsp--ensure-backend)
+  (lean-lsp--request-async lean-lsp--backend :$/lean/plainTermGoal
+                           #'lean-infoview--term-goal callback))
 
 ;;;; Association:
 
